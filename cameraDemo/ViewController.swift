828296e684ec0589fb22b404d812bdbb247ebc81
MANUEL HERNANDEZ <rl106926@rlas-macair-36.local>
2018-12-04 11:13:15 -0600

    Initial Commit

---
+++ cameraDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cameraDemo
+//
+//  Created by MANUEL HERNANDEZ on 12/4/18.
+//  Copyright © 2018 MANUEL HERNANDEZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
